94e005394d6891e9abce10c16bec07fbbecaf767
Michael Rasolonjatovo <rasolon.michael@gmail.com>
2024-01-29 16:26:42 +0100

    <feat> CORS Setting + Routes on mock api

--- server/app.js
+++ server/app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const PORT = 5005;
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
 // ...
-
+const cohorts = require('./cohorts.json')
+const students = require('./students.json')
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
@@ -20,6 +22,7 @@ app.use(morgan("dev"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 
 
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
@@ -28,7 +31,12 @@ app.use(cookieParser());
 app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
-
+app.get("/api/cohorts", (req, res) => {
+  res.json(cohorts);
+});
+app.get("/api/students", (req, res) => {
+  res.json(students);
+});
 
 // START SERVER
 app.listen(PORT, () => {
