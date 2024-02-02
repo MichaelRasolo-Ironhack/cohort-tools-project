1a9e8fa74dacf4d6d7a182a4bfecf3d390efb4a9
doncarlos5 <pro.julien.thomas@gmail.com>
2024-02-02 17:27:58 +0100

    isAuth file

---
+++ server/middleware/isAuthenticated.js
@@ -0,0 +1,9 @@
+const User = require("../models/User.model");
+const jwt = require("jsonwebtoken");
+
+async function isAuthenticated(req, res, next) {
+  try {
+  } catch (error) {
+    next(error);
+  }
+}
